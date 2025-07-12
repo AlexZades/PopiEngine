511c17b6e6462b0605013906a0503dbac8c86fda
Alex <az@st4r.io>
2025-07-13 07:59:19 +0900

    updated the ECS

--- popi/engine/include/components.h
+++ popi/engine/include/components.h
@@ -3,22 +3,39 @@
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 
-struct Transform {
-	glm::vec3 position; 
-	glm::vec3 rotation; 
-	glm::vec3 scale;
-};
+/// <summary>
+/// I wanted to make a simple ECS system for PopiEngine.
+/// The way the system works is each entity can have multiple components.
+/// Each component is a struct that contains data relevant to that component.
+///  The components are stored in a bitmask, so we can easily check which components are active.
+/// </summary>
+namespace PopiEngine::ECS {
 
-struct MeshRenderer {
+	enum ActiveComponents {
+		TRANSFORM = 1 << 0, // 0001
+		MESH_RENDERER = 1 << 1, // 0010
+		DIRECTIONAL_LIGHT = 1 << 2 // 0100
+	};
 
-};
+	struct Transform {
+		glm::vec3 position;
+		glm::vec3 rotation;
+		glm::vec3 scale;
+	};
 
-struct Camera {
+	struct MeshRenderer {
+		GLuint meshID;
+	};
 
-};
 
-struct Light {
 
-};
+	struct DirectionalLight {
+		glm::vec3 direction;
+
+		glm::vec3 ambient;
+		glm::vec3 diffuse;
+		glm::vec3 specular;
+	};
+}
 
 
--- popi/engine/include/entity.h
+++ popi/engine/include/entity.h
@@ -1,7 +1,36 @@
-#pragma once
+#pragma once  
+#include <iostream>  
+#include <components.h>  
+using namespace PopiEngine::ECS;  
+using std::string;
 
+namespace PopiEngine::ECS {  
+      
+    class Entity {
+    public:
+		string name;
+        std::shared_ptr<Transform> transform;
+        std::shared_ptr<MeshRenderer> meshRenderer;
+        std::shared_ptr<DirectionalLight> directionalLight;
 
-namespace PopiEngine::ECS {
-
-
+        Entity(string _name = "") {
+			name = _name;
+        }
+        ~Entity(){
+        
+        }
+  
+        void AttachTransform() {  
+            transform = std::make_shared<Transform>();  
+        }  
+        void AttachMesh() {  
+            meshRenderer = std::make_shared<MeshRenderer>();  
+        }  
+        void AttachDirectionalLight() {  
+            directionalLight = std::make_shared<DirectionalLight>();  
+        }  
+    private:   
+        ActiveComponents attachedComponents;  
+  
+    };  
 }
